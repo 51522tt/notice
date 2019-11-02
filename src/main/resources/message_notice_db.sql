17a184c83db7b10e29ac5200c18ff7cb98bd9679
MoMo <1210217980@qq.com>
2019-11-02 18:08:00 +0800

    修改SQL文件

--- src/main/resources/message_notice_db.sql
+++ src/main/resources/message_notice_db.sql
@@ -23,12 +23,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `log_notice_sending_record`;
 CREATE TABLE `log_notice_sending_record` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `log_suorce_data` text COMMENT '原始数据',
+  `log_source_data` text COMMENT '原始数据',
   `notice_template_number` int(10) DEFAULT NULL COMMENT '通知模板编号',
   `log_send_data` text COMMENT '拼装好后发送的信息',
-  `log_send_user` varchar(255) DEFAULT NULL COMMENT '发送人',
+  `log_sender` varchar(255) DEFAULT NULL COMMENT '发送人',
   `log_send_ip` varchar(255) DEFAULT NULL COMMENT '发送IP',
-  `log_receive_user` varchar(255) DEFAULT NULL COMMENT '接收人(多个用''|''表示)',
+  `log_receiver` varchar(255) DEFAULT NULL COMMENT '接收人(多个用''|''表示)',
   `log_receive_department` varchar(255) DEFAULT NULL COMMENT '接收部门(多个用''|''表示)',
   `log_cc` varchar(255) DEFAULT NULL COMMENT '抄送人（好像没什么卵用）',
   `log_send_datetime` datetime(6) NOT NULL COMMENT '发送时间',
